8ec4537c05b0d1936896a6818c9cf95e4030657c
baier <evd.matvey@mail.ru>
2024-03-31 20:09:25 +0700

    Add update work time component

---
+++ src/renderer/features/UpdateWorkTime/ui/index.tsx
@@ -0,0 +1,22 @@
+import { SettingsItem, useSettingsStore, useUpdateTime } from '../../../entities/Settings';
+import { Input } from '../../../shared/UI';
+
+const UpdateWorkTime = () => {
+  const workSessionTime = useSettingsStore((state) => state.workSessionTime);
+  const setWorkSessionTime = useSettingsStore((state) => state.setWorkSessionTime);
+
+  const { currentTime, isError, updateTimeHandler } = useUpdateTime(
+    workSessionTime,
+    setWorkSessionTime,
+  );
+
+  return (
+    <div>
+      <SettingsItem title="Work time" description="Set a value from 1 to 60" isError={isError}>
+        <Input placeholder="45" value={currentTime} onChange={updateTimeHandler} />
+      </SettingsItem>
+    </div>
+  );
+};
+
+export default UpdateWorkTime;
