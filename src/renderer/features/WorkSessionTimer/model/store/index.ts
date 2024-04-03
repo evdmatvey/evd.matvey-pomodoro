6eeab4d74d971694b997746007443c3cc948449a
baier <evd.matvey@mail.ru>
2024-04-03 10:54:30 +0700

    Add work session timer

---
+++ src/renderer/features/WorkSessionTimer/model/store/index.ts
@@ -0,0 +1,36 @@
+import { create } from 'zustand';
+
+interface WorkSessionTimerState {
+  isStopped: boolean;
+  isEnd: boolean;
+  completeWorkSession: () => void;
+  pauseWorkSession: () => void;
+  continueWorkSession: () => void;
+  resetWorkSession: () => void;
+}
+
+export const useWorkSessionTimerStore = create<WorkSessionTimerState>()((set) => ({
+  isStopped: false,
+  isEnd: false,
+  completeWorkSession: () =>
+    set((state) => ({
+      ...state,
+      isEnd: true,
+    })),
+  pauseWorkSession: () =>
+    set((state) => ({
+      ...state,
+      isStopped: true,
+    })),
+  continueWorkSession: () =>
+    set((state) => ({
+      ...state,
+      isStopped: false,
+    })),
+  resetWorkSession: () =>
+    set((state) => ({
+      ...state,
+      isEnd: false,
+      isStopped: false,
+    })),
+}));
